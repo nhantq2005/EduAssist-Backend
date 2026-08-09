74ed1186e82c27014450ee104b9f5d334da29c25
nhantq <nhan.tq2005@gmail.com>
2026-08-09 21:34:27 +0700

    create api for stats

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
 
-from app.routers import user, subject, enrollment, question, option, quiz
-from app.routers.document import router as document_router
+from app.routers import user_router, subject_router, question_router, option_router, quiz_router, chat_router, \
+    stats_router
+from app.routers.document_router import router as document_router
 
 from app.core.config import settings
+from app.admin.admin import setup_admin
 
 app = FastAPI()
 
 settings.configure_cloudinary()
 
+setup_admin(app)
+
 
 app.include_router(document_router, prefix="/api")
-app.include_router(user.router, prefix="/api")
-app.include_router(subject.router, prefix="/api")
-app.include_router(enrollment.router, prefix="/api")
-app.include_router(question.router, prefix="/api")
+app.include_router(user_router.router, prefix="/api")
+app.include_router(subject_router.router, prefix="/api")
+app.include_router(question_router.router, prefix="/api")
 # app.include_router(option.router, prefix="/api")
-app.include_router(quiz.router, prefix="/api")
-
+app.include_router(quiz_router.router, prefix="/api")
+app.include_router(chat_router.router, prefix="/api/chat")
+app.include_router(stats_router.router, prefix="/api")
 
 @app.get("/")
 async def root():
