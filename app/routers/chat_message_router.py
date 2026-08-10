860397bfd4e6991e483be7fcd2b5a72b00576edd
nhantq <nhan.tq2005@gmail.com>
2026-08-10 22:19:07 +0700

    update permission for api

--- app/routers/chat_message_router.py
+++ app/routers/chat_message_router.py
@@ -2,12 +2,14 @@ from typing import List
 from fastapi import APIRouter, Depends
 
 from app.api.dependencies import get_chat_message_service
+from app.core.permissions import require_role
 from app.schemas.chat_message import ChatMessageResponse
 from app.services.chat_message_service import ChatMessageService
 
 router = APIRouter(tags=["ChatMessage"])
 
 @router.get("/chat-sessions/{chat_session_id}/chat-message", response_model=List[ChatMessageResponse])
+@require_role(["ADMIN", "LECTURE", "STUDENT"])
 async def get_chat_message_by_chat_session_id(
         chat_session_id: int,
         chat_message_service: ChatMessageService = Depends(get_chat_message_service)
--- app/routers/chat_router.py
+++ app/routers/chat_router.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
 from app.api.dependencies import get_rag_service
+from app.core.permissions import require_role
 from app.schemas.chat_message import ChatMessageRequest
 from app.services.rag_service import RagService
 
 router = APIRouter(tags=["Chat"])
 
 @router.post("/chat/stream")
+@require_role(["ADMIN", "LECTURE", "STUDENT"])
 async def chat_stream(request: ChatMessageRequest, rag_service : RagService = Depends(get_rag_service)):
     # return StreamingResponse(
     #     stream_answer(request.question, request.chat_session_id, db),
