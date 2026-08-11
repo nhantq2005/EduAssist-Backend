972c28e8876b544ff0f19fc33a3c398993e10045
nhantq <nhan.tq2005@gmail.com>
2026-08-12 05:29:10 +0700

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.routers import user_router, subject_router, question_router, quiz_router, chat_router, \
-    stats_router
+    stats_router, chat_message_router, chat_session_router
 from app.routers.document_router import router as document_router
 
 from app.core.config import settings
@@ -21,14 +21,6 @@ app.include_router(question_router.router, prefix="/api")
 app.include_router(quiz_router.router, prefix="/api")
 app.include_router(chat_router.router, prefix="/api")
 app.include_router(stats_router.router, prefix="/api")
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
+app.include_router(chat_message_router.router, prefix="/api")
+app.include_router(chat_session_router.router, prefix="/api")
 
