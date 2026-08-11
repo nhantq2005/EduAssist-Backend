1f31a5c192d95643a203619860ae3205938a2133
nhantq <nhan.tq2005@gmail.com>
2026-08-12 05:02:33 +0700

    update permission for chat API

--- app/routers/chat_router.py
+++ app/routers/chat_router.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter, Depends
-from app.api.dependencies import get_rag_service
+from app.api.dependencies import get_rag_service, get_current_user
 from app.core.permissions import require_role
+from app.models.user import User, UserRole
 from app.schemas.chat_message import ChatMessageRequest
 from app.services.rag_service import RagService
 
 router = APIRouter(tags=["Chat"])
 
 @router.post("/chat/stream")
-@require_role(["ADMIN", "LECTURE", "STUDENT"])
-async def chat_stream(request: ChatMessageRequest, rag_service : RagService = Depends(get_rag_service)):
+@require_role(["ADMIN", "LECTURER", "STUDENT"])
+async def chat_stream(
+    request: ChatMessageRequest, 
+    current_user: User = Depends(get_current_user),
+    rag_service : RagService = Depends(get_rag_service)
+):
     # return StreamingResponse(
     #     stream_answer(request.question, request.chat_session_id, db),
     #     media_type="text/event-stream"
