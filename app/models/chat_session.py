a0ba5c5dc919832b3a737f69ecec04f3aaff4939
nhantq <nhan.tq2005@gmail.com>
2026-08-15 14:10:50 +0700

    add field created_date and updated_date for chat_session model

--- app/models/chat_session.py
+++ app/models/chat_session.py
@@ -10,8 +10,8 @@ class ChatSession(Base):
     __tablename__ = "chat_sessions"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(nullable=False)
-    created_date: Mapped[datetime] = mapped_column(nullable=False)
-    updated_date: Mapped[datetime] = mapped_column(nullable=False)
+    created_date: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    updated_date: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now, nullable=False)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
 
     user: Mapped["User"] = relationship(back_populates="chat_sessions")
