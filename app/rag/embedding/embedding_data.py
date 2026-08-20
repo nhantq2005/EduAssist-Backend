ab318c048916741129901e6e0e0ea93ef381467e
nhantq <nhan.tq2005@gmail.com>
2026-08-20 16:26:16 +0700

    update: use model from singleton

--- app/rag/embedding/embedding_data.py
+++ app/rag/embedding/embedding_data.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from typing import List
-
-import torch
 from langchain_core.documents import Document
 from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+from app.rag.model import rag_models_instance
 
 
 def embed_and_save_to_chroma(documents: List[Document], collection_name: str = "data-collection"):
@@ -16,17 +14,8 @@ def embed_and_save_to_chroma(documents: List[Document], collection_name: str = "
         print("Document không có dữ liệu")
         return
 
-    model_name = "BAAI/bge-m3"
-    print(f"EMBEDDING MODEL: {model_name}")
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    # KHOI TAO MODEL EMBEDDING
-    embeddings = HuggingFaceEmbeddings(
-        model_name=model_name,
-        model_kwargs={'device': device},
-        encode_kwargs={'normalize_embeddings': True}
-    )
+    # KHOI TAO MODEL EMBEDDING TU SINGLETON
+    embeddings = rag_models_instance.embeddings
 
     print(f"Đang thêm {len(documents)} chunks mới vào ChromaDB")
 
