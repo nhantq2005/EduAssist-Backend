1ed771b87c03eee3cf22f3a86c14af1faf17c072
nhantq <nhan.tq2005@gmail.com>
2026-08-20 11:45:41 +0700

    create singleton class to load model

---
+++ app/rag/model.py
@@ -0,0 +1,46 @@
+import torch
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+import pickle
+
+
+class RAGModels:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(RAGModels, cls).__new__(cls)
+            
+            cls._instance.embeddings = HuggingFaceEmbeddings(
+                model_name="BAAI/bge-m3",
+                model_kwargs={'device': 'cpu'},
+                encode_kwargs={'normalize_embeddings': True}
+            )
+
+            device_reranker = "cuda" if torch.cuda.is_available() else "cpu"
+            cls._instance.cross_encoder = HuggingFaceCrossEncoder(
+                model_name="BAAI/bge-reranker-v2-m3",
+                model_kwargs={
+                    'device': device_reranker,
+                    'automodel_args': {'torch_dtype': torch.float16} if device_reranker == "cuda" else {}
+                }
+            )
+
+            from pathlib import Path
+            import os
+            CURRENT_FILE = Path(__file__).resolve()
+            ROOT_DIR = CURRENT_FILE.parents[2]
+            bm25_save_path = ROOT_DIR / "bm25_index.pkl"
+            if os.path.exists(bm25_save_path):
+                with open(bm25_save_path, 'rb') as f:
+                    cls._instance.bm25_retriever = pickle.load(f)
+            else:
+                print(f"Không tìm thấy file {bm25_save_path}. Hãy chạy script build dữ liệu trước.")
+                cls._instance.bm25_retriever = None
+
+            print("Tải mô hình thành công")
+
+        return cls._instance
+
+
+rag_models_instance = RAGModels()
