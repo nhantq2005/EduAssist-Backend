d9b75f6529e6c87679ed4997912113f9eff824d4
nhantq <nhan.tq2005@gmail.com>
2026-08-26 23:20:25 +0700

    update BGE-M3 use CPU

--- app/utils/cloudinary_utils.py
+++ app/utils/cloudinary_utils.py
@@ -2,10 +2,9 @@ import cloudinary.uploader
 from fastapi import UploadFile
 from fastapi.concurrency import run_in_threadpool
 
+
 async def upload_file_to_cloudinary(file: UploadFile, folder: str = "documents") -> dict:
-    """
-    Uploads a file to Cloudinary and returns the response dictionary.
-    """
+
     result = await run_in_threadpool(
         cloudinary.uploader.upload,
         file.file,
