2a680afeb1dc90ae54555ed5c8f37b08f409c13a
nhantq <nhan.tq2005@gmail.com>
2026-09-04 00:12:04 +0700

    update hybrid_retriever.py

--- app/rag/generate/hybrid_retriever.py
+++ app/rag/generate/hybrid_retriever.py
@@ -1,27 +1,19 @@
-from pathlib import Path
 from langchain_classic.retrievers import ContextualCompressionRetriever, EnsembleRetriever
 from langchain_classic.retrievers.document_compressors import CrossEncoderReranker
 from app.rag.model import rag_models_instance
 
 def get_hybrid_reranked_retriever(top_k: int = 3):
-    CURRENT_FILE = Path(__file__).resolve()
-    ROOT_DIR = CURRENT_FILE.parents[3] if len(CURRENT_FILE.parents) > 3 else CURRENT_FILE.parent
-    chroma_db_dir = ROOT_DIR / "chroma_db"
-
-    # LAY EMBEDDING TỪ SINGLETON
-    embeddings = rag_models_instance.embeddings
     vectorstore = rag_models_instance.vectorstore
-
     print("Công thức tính độ tương đồng của Chroma:", vectorstore._collection.metadata)
-    # TRUY XUAT TU CHROMA
     chroma_retriever = vectorstore.as_retriever(search_kwargs={"k": top_k * 2})
 
-    # LAY BM25 TỪ SINGLETON
     bm25_retriever = rag_models_instance.bm25_retriever
-    bm25_retriever.k = top_k * 2
-    base_retriever = EnsembleRetriever(retrievers=[bm25_retriever, chroma_retriever], weights=[0.5, 0.5])
+    if bm25_retriever is not None:
+        bm25_retriever.k = top_k * 3
+        base_retriever = EnsembleRetriever(retrievers=[bm25_retriever, chroma_retriever], weights=[0.5, 0.5])
+    else:
+        base_retriever = chroma_retriever
 
-    # XEP HANG NGU CANH
     cross_encoder = rag_models_instance.cross_encoder
     compressor = CrossEncoderReranker(model=cross_encoder, top_n=top_k)
     compression_retriever = ContextualCompressionRetriever(base_compressor=compressor,base_retriever=base_retriever)
